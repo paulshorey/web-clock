23c835ef53fa7e38a9163f29e63100b8847d9915
paul (local) <ps@artspaces.net>
2017-12-18 21:39:10 -0700

    merged: now WindowX is changing the Top header title - Redux not necessary for routing and UI content

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,6 @@ import {applyMiddleware, createStore, combineReducers}  from "redux";
 import logger from "redux-logger";
 
 import * as reducers from "redux/reducers";
-console.log('reducers',reducers);
 
 const createStoreWithMiddleware = applyMiddleware(logger)(createStore);
 const reducer = combineReducers(reducers);
